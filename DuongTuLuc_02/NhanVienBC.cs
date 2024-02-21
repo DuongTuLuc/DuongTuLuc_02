e5435d41195e975cbd6f4c8d5203c89ae5f7f6e7
ADMIN <ADMIN@PC321>
2024-02-21 11:15:18 +0700

    Add project files.

---
+++ DuongTuLuc_02/NhanVienBC.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuongTuLuc_02
+{
+    class NhanVienBC:NhanVien
+    {
+        private  string mucxeploai;
+
+        public NhanVienBC():base()
+        { }
+
+        public NhanVienBC(string maso, string hoten, int luongcung) : base(maso,hoten,luongcung)
+        {
+            this.mucxeploai = mucxeploai;
+        }
+
+        public string MucXeploai
+        {
+            set { this.mucxeploai = value; }
+            get { return mucxeploai; }
+        }
+
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap xep loai:");
+            this.mucxeploai = Console.ReadLine();
+        }
+
+        public override double TinhLuong()
+        {
+            double luong;
+            if (this.mucxeploai == "a")
+            {
+                 luong = 1.5 * Luongcung;
+            }
+            else if (this.mucxeploai == "b")
+            {
+                 luong = 1.0 * Luongcung;
+            }
+            else
+            {
+                 luong = 0.5 * Luongcung;
+            }
+            return luong;
+        }
+    }
+}
---
+++ DuongTuLuc_02/NhanVienHD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuongTuLuc_02
+{
+    class NhanVienHD:NhanVien
+    {
+        private int doanhthu;
+
+        public NhanVienHD():base()
+        {
+
+        }
+
+        public NhanVienHD(string maso, string hoten, int luongcung) : base(maso, hoten, luongcung)
+        {
+            this.doanhthu = doanhthu;
+        }
+
+        public int DoanhThu
+        {
+            set { this.doanhthu = value; }
+            get { return doanhthu; }
+        }
+
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap doanh thu:");
+            this.doanhthu = int.Parse(Console.ReadLine());
+        }
+        public override double TinhLuong()
+        {
+            double luong = 0.1 * doanhthu;
+            return (int)luong;
+        }
+    }
+}
